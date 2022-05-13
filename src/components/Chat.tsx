927f45601d14a3e7e4f895f87600e4e6e0527043
yuta19910929 <seventh.heaven0929@gmail.com>
2022-05-14 00:44:05 +0900

    TypeScript化(component:Chat)

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -3,14 +3,15 @@ import SignOut from './SignOut';
 import SendMessage from './SendMessage';
 import { auth, db } from '../firebase.js';
 
-const Chat = () => {
+const Chat: React.VFC = (messageData) => {
 
   //firebaseからデータを取得
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<any[]>([]);
+
   useEffect(() => {
     db.collection("messages")
       .orderBy("createdAt")
-      .limit(50)
+      .limit(30)
       .onSnapshot((snapshot) => {
         setMessages(snapshot.docs.map((doc) => doc.data()));
     });
@@ -20,15 +21,15 @@ const Chat = () => {
     <div>
       <header>
         <h1>Lime</h1>
-        <h2>{auth.currentUser.displayName}でログイン中</h2>
+        <h2>{auth.currentUser?.displayName}でログイン中</h2>
         <SignOut />
       </header>
       <div className="msgs">
-        {messages.map(({id, text, photoURL, uid}) => (
-          <div className={`${uid === auth.currentUser.uid ? "taR" : "taL"}`}>
+        {messages.map(({ text, photoURL, uid }) => (
+          <div className={`${uid === auth.currentUser?.uid ? "taR" : "taL"}`}>
             <div
-              key={id}
-              className={`msg ${uid === auth.currentUser.uid ? "send" : "received"}`}>
+              key={uid}
+              className={`msg ${uid === auth.currentUser?.uid ? "send" : "received"}`}>
                 <img src={photoURL} alt="acountPhoto" />
                 <p>{text}</p>
           </div>
